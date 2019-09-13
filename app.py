8d7dbb6bf1bf563036798a3ea64507e8665e322f
marwanihab <marwan.ihab95@gmail.com>
2019-09-13 22:29:51 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, session
+app = Flask(__name__)
+@app.route("/<string:name>")
+def customHello(name):
+    return ""
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", headline="Hellooooooo")
+
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/notes", methods=["GET,POST"])
+def notes():
+    if session.get("notes") == []:
+        note = request.form.get("note")
+        session["notes"].append(note)
+
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
+
+    return render_template("route.html", notes=session["notes"])
